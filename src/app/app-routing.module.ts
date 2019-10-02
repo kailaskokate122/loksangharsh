5eb5e5d124d179a6eeae90db6b92a4c60311f6b6
kailaskokate122 <kailaskokate122@gmail.com>
2019-10-02 19:54:29 +0530

    add home header files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}, {
+  path: 'home',
+  component: HomeComponent
+}, {
+  path: "page-not-found",
+  component: PageNotFoundComponent
+}, {
+  path: "404",
+  component: PageNotFoundComponent
+}, {
+  path: "**",
+  redirectTo: "404"
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
